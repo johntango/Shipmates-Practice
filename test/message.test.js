134857940ffd7a985a639c3bbd0908c38580121c
digitaltango <jrwtango@gmail.com>
2026-07-13 13:28:42 -0400

    Add optional ship assignment

--- test/message.test.js
+++ test/message.test.js
@@ -10,3 +10,10 @@ test("returns a message for the default crewmate", () => {
 test("includes a supplied crewmate name", () => {
   assert.equal(practiceMessage("Scout"), "Welcome aboard, Scout.");
 });
+
+test("optionally includes a ship assignment", () => {
+  assert.equal(
+    practiceMessage("Scout", "Endeavour"),
+    "Welcome aboard, Scout. You are assigned to Endeavour.",
+  );
+});
